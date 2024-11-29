0be3bcaecd62f337a4bf9e4528a501d33d454173
labuser <m.surewicz@wp.pl>
2024-11-29 09:25:41 +0100

    Maria

--- src/main/java/eu/chrost/git/Main.java
+++ src/main/java/eu/chrost/git/Main.java
@@ -2,6 +2,6 @@ package eu.chrost.git;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello Marcin!");
+        System.out.println("Hello Marcin:)!");
     }
 }
