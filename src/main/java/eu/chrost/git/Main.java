e1f131c111fc6298cb7d3b36bb632be49a6cfbbd
Maria8018 <M.surewicz@wp.pl>
2024-11-29 09:42:18 +0100

    Update Main.java

--- src/main/java/eu/chrost/git/Main.java
+++ src/main/java/eu/chrost/git/Main.java
@@ -1,7 +1,12 @@
 package eu.chrost.git;
-
+import java.util.Random;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello Marcin:)!");
+        Random rand = new Random();
+
+int value = rand.nextInt(100);
+
+System.out.println(value);
     }
 }
